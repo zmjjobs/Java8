9de83d428b3683633e24579a5bafa2dd9326a570
zmjjobs <dream98job@126.com>
2020-02-14 23:40:28 +0800

    Stream创建、中间操作、终止操作

--- src/com/isoftstone/bean/Employee.java
+++ src/com/isoftstone/bean/Employee.java
@@ -5,6 +5,7 @@ public class Employee {
 	private String name;
 	private int age;
 	private double salary;
+	private Status status;
 	public Employee() {
 	}
 	public Employee(int id) {
@@ -24,6 +25,13 @@ public class Employee {
 		this.name = name;
 		this.age = age;
 		this.salary = salary;
+	}
+	
+	public Employee(String name, int age, double salary, Status status) {
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+		this.status = status;
 	}
 	public int getId() {
 		return id;
@@ -49,9 +57,63 @@ public class Employee {
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
+	
+	
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(salary);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (age != other.age)
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
+			return false;
+		if (status != other.status)
+			return false;
+		return true;
+	}
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", salary=" + salary + "]";
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", salary=" + salary + ", status=" + status
+				+ "]";
 	}
-	
+
+
+	public enum Status{
+		FREE,
+		BUSY,
+		VOCATION;
+	}
 }
